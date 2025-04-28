12d581eb90da55f5beb40c8c3e544f4b5cd3968f
yasser ramdani <139624089+IkhMoh@users.noreply.github.com>
2025-04-28 17:55:25 +0100

    code improvement foor header

--- Frontend/src/components/NavBar.tsx
+++ Frontend/src/components/NavBar.tsx
@@ -3,13 +3,13 @@ import { MdOutlineShoppingCart } from "react-icons/md";
 import Image from "next/image";
 const NavBar = () => {
   return (
-    <header className="bg-orange-200 shadow-2xl">
-      <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
-        <div className="flex h-16 items-center justify-between ">
+    <header className="bg-orange-100 shadow-2xl ">
+      <div className="max-w-screen-xl px-4 sm:px-6 lg:px-8 container mx-auto">
+        <div className="flex  h-16 items-center justify-between">
           {/* the firs bart */}
-          <div className="flex justify-between items-center gap-4 md:gap-0 h-full ">
+          <div className="flex flex-1 justify-around   items-center gap-4 md:gap-0 h-full  ">
             {/* Image */}
-            <div className="flex-1 md:flex md:items-center md:gap-12">
+            <div className="flex md:flex md:items-center md:gap-12">
               <a className="p-0 m-0 overflow-hidden" href="#">
                 <Image
                   src="/logo.png"
@@ -24,29 +24,24 @@ const NavBar = () => {
             {/* Link */}
             <div className="md:flex md:items-center md:gap-12">
               <nav aria-label="Global" className="hidden md:block">
-                <ul className="flex items-center gap-6 text-sm front-bold c">
+                <ul
+                  className="flex items-center gap-6 text-sm font-medium
+"
+                >
                   <li>
-                    <a className="text-gray-500 transition hover:text-gray-500/75 cursor-pointer">
-                      About Us
-                    </a>
+                    <a className="  transition   cursor-pointer">About Us</a>
                   </li>
 
                   <li>
-                    <a className="text-gray-500 transition hover:text-gray-500/75 cursor-pointer">
-                      Menu
-                    </a>
+                    <a className=" transition cursor-pointer">Menu</a>
                   </li>
 
                   <li>
-                    <a className="text-gray-500 transition hover:text-gray-500/75 cursor-pointer">
-                      Shop
-                    </a>
+                    <a className=" transition cursor-pointer">Shop</a>
                   </li>
 
                   <li>
-                    <a className="text-gray-500 transition hover:text-gray-500/75 cursor-pointer">
-                      Contact Us
-                    </a>
+                    <a className=" transition cursor-pointer">Contact Us</a>
                   </li>
                 </ul>
               </nav>
@@ -54,50 +49,48 @@ const NavBar = () => {
             {/* Link=========== */}
           </div>
           {/* the firs bart========== */}
-
           {/* the second bart */}
-          <div className=" flex justify-between md:items-center md:gap-4 h-full ">
+          <div className=" flex flex-1 justify-around md:items-center md:gap-4 h-full  ">
             {/* search bart */}
 
             <div className="flex items-center justify-center  p-4">
               <div className="flex w-80 ">
                 <input
-                  
                   type="text"
                   value="hi"
                   placeholder="🔍 Search..."
-                  className="w-64 px-2 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-100 focus:w-96 transition-all"
+                  className="w-64 px-2 py-2 rounded-lg border-2 h-9 focus:w-96 transition-all"
                 />
               </div>
             </div>
             {/* search bart========= */}
-            {/* icon bart */} 
+            {/* icon bart */}
 
             <div className="flex items-center gap-4">
               <div className="sm:flex sm:gap-4">
-                <MdOutlineShoppingCart className="cursor-pointer" />
+                <MdOutlineShoppingCart className="cursor-pointer w-6 h-6" />
 
                 <div className="hidden sm:flex">{/* imgae profile */}</div>
               </div>
               {/* for the md size */}
               {/* <div className="block md:hidden">
-                <button className="rounded-sm bg-gray-100 p-2 text-gray-600 transition hover:text-gray-600/75">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="size-5"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M4 6h16M4 12h16M4 18h16"
-                    />
-                  </svg>
-                </button>
-              </div> */}
+                           <button className="rounded-sm bg-gray-100 p-2 text-gray-600 transition hover:text-gray-600/75">
+                             <svg
+                               xmlns="http://www.w3.org/2000/svg"
+                               className="size-5"
+                               fill="none"
+                               viewBox="0 0 24 24"
+                               stroke="currentColor"
+                               strokeWidth="2"
+                             >
+                               <path
+                                 strokeLinecap="round"
+                                 strokeLinejoin="round"
+                                 d="M4 6h16M4 12h16M4 18h16"
+                               />
+                             </svg>
+                           </button>
+                         </div> */}
             </div>
             {/* icon bart========== */}
           </div>
