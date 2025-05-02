9d6b73818278bd24c6ced06fed9a00c0ad7f9443
yasser ramdani <139624089+IkhMoh@users.noreply.github.com>
2025-05-02 23:57:33 +0100

    improvement for the code

--- Frontend/src/app/products/[hi]/page.tsx
+++ Frontend/src/app/products/[hi]/page.tsx
@@ -3,8 +3,8 @@ import React from "react";
 
 function page() {
   return (
-    <div className="lg:grid lg:grid-cols-4  lg:gap-3 w-full bg-orange-200 min-h-screen overflow-y-auto">
-      <div className="lg:col-span-12 mt-2 w-full">
+    <div className="lg:grid lg:grid-cols-4  lg:gap-3 w-full bg-orange-200 min-h-9/12 overflow-y-auto">
+      <div className="lg:col-span-12 mt-2 ">
         <Workspace />
       </div>
     </div>
--- Frontend/src/components/Workspace.tsx
+++ Frontend/src/components/Workspace.tsx
@@ -5,7 +5,7 @@ function Workspace() {
   return (
     <div>
       <h1 className="text-2xl font-bold ml-4">Breakfast Menu</h1>
-      <ul className="grid gap-2 sm:grid-cols-2 lg:grid-cols-4 px-6 items-center justify-items-center my-6">
+      <ul className="grid gap-2 sm:grid-cols-2 lg:grid-cols-4 px-6 items-center justify-items-center my-6 overflow-y-auto h-screen">
         <Card />
         <Card />
         <Card />
