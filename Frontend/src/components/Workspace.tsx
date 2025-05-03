5106d1c83e44373123499076d25e6b7e3c922ced
yasser ramdani <139624089+IkhMoh@users.noreply.github.com>
2025-05-03 19:48:40 +0100

    improvement for workspace page

--- Frontend/src/components/Workspace.tsx
+++ Frontend/src/components/Workspace.tsx
@@ -3,9 +3,9 @@ import Card from "./Card";
 
 function Workspace() {
   return (
-    <div>
-      <h1 className="text-2xl font-bold ml-4">Breakfast Menu</h1>
-      <ul className="grid gap-2 sm:grid-cols-2 lg:grid-cols-4 px-6 items-center justify-items-center my-6 overflow-y-auto h-screen">
+    <div className="flex flex-col items-center justify-center w-full h-screen">
+      <h1 className="text-2xl font-bold ml-4 ">Breakfast Menu</h1>
+      <ul className="grid gap-2 sm:grid-cols-2 lg:grid-cols-4 px-6 items-center justify-items-center my-6 overflow-y-auto ">
         <Card />
         <Card />
         <Card />
