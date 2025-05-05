6886d0f5f7bc436bf03653e6045a6ed1a3ddaeab
yasser ramdani <139624089+IkhMoh@users.noreply.github.com>
2025-05-05 16:57:09 +0100

    improvement

--- Frontend/src/components/Categorycard.tsx
+++ Frontend/src/components/Categorycard.tsx
@@ -6,7 +6,7 @@ function Categorycard() {
     <Link href="/products/Breakfast">
       <div className="group relative block bg-black">
         <Image
-          width={300}
+          width={300} 
           height={300}
           alt=""
           src="https://images.unsplash.com/photo-1603871165848-0aa92c869fa1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=772&q=80"
