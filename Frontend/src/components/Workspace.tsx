ccfddfa6636b8e5ca2d51359f04f8d12e85d6803
yasser ramdani <139624089+IkhMoh@users.noreply.github.com>
2025-05-05 18:55:02 +0100

    improvement

--- Frontend/src/components/Workspace.tsx
+++ Frontend/src/components/Workspace.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Card from "./Card";
 
-// ممكن تعريف نوع product هنا بدل any إذا تحب
 async function Workspace() {
   const res = await fetch("http://localhost:1337/api/products?populate=*");
   const data = await res.json();
@@ -12,7 +11,7 @@ async function Workspace() {
       <h1 className="text-2xl font-bold ml-4">Breakfast Menu</h1>
       <ul className="grid gap-2 sm:grid-cols-2 lg:grid-cols-4 px-6 items-center justify-items-center my-6 overflow-y-auto">
         {data.data.map((product: any) => {
-          const images = product.image ;
+          const images = product.image;
 
           return (
             <Card
