f7010db9240037cb76f70d97c71e4d13a95d4ec6
yasser ramdani <139624089+IkhMoh@users.noreply.github.com>
2025-05-05 18:55:33 +0100

    Getting the data from the API

--- Frontend/src/components/Card.tsx
+++ Frontend/src/components/Card.tsx
@@ -35,29 +35,7 @@ function Card({ image, price, title }: CardProps) {
             </div>
           </dl>
 
-          <div className="mt-6 flex items-center gap-8 text-xs">
-            <div className="sm:inline-flex sm:shrink-0 sm:items-center sm:gap-2">
-              <svg
-                className="size-4 text-indigo-700"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M8 14v3m4-3v3m4-3v3M3 21h18M3 10h18M3 7l9-4 9 4M4 10h16v11H4V10z"
-                />
-              </svg>
-
-              <div className="mt-1.5 sm:mt-0">
-                <p className="text-gray-500">Parking</p>
-                <p className="font-medium">2 spaces</p>
-              </div>
-            </div>
-          </div>
+        
         </div>
       </a>
     </div>
