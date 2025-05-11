2abbfe53e45a282ea79e84b3acd43d3dcd5bcc15
yasser ramdani <139624089+IkhMoh@users.noreply.github.com>
2025-05-11 19:26:52 +0100

    improvement for buttons

--- Frontend/src/components/Card.tsx
+++ Frontend/src/components/Card.tsx
@@ -58,14 +58,15 @@ function Card({ image, price, title, rating, params }: CardProps) {
               </div>
               <div className=" flex items-center justify-center ">
                 <a
-                  className="group relative inline-block overflow-hidden border border-orange-400 px-4 py-1 rounded-4xl focus:ring-3 focus:outline-hidden"
+                  className="group relative inline-block overflow-hidden border border-black px-4 py-1 rounded-4xl focus:ring-3 focus:outline-hidden"
                   href="#"
                 >
-                  <span className="absolute inset-y-0 left-0 w-[0px] bg-orange-400 transition-all group-hover:w-full"></span>
+                  <span className="absolute inset-y-0 left-0 w-[0px] bg-black transition-all group-hover:w-full"></span>
 
-                  <span className="relative text-sm font-bold  text-orange-400 transition-colors group-hover:text-white">
+                  <span className="relative text-sm font-bold  text-black transition-colors group-hover:text-white">
                     Add Item
                   </span>
+                  bu
                 </a>
               </div>
             </div>
