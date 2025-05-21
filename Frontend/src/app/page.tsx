e184df89d642634980c9eefd59563db67a5bf7ff
yasser ramdani <139624089+IkhMoh@users.noreply.github.com>
2025-05-21 19:17:25 +0100

    improvement for the allproduct page and add the contact page

--- Frontend/src/app/page.tsx
+++ Frontend/src/app/page.tsx
@@ -1,5 +1,6 @@
 import Allproducts from "@/components/Allproducts";
 import Categorysection from "@/components/Categorysection";
+import Contact from "@/components/Contact";
 import Hero from "@/components/Hero";
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Categorysection />
       <Allproducts />
       <Categorysection />
+      <Contact/>
     </main>
   );
 }
--- Frontend/src/components/Allproducts.tsx
+++ Frontend/src/components/Allproducts.tsx
@@ -6,8 +6,18 @@ function Allproducts() {
     <div className="my-20">
       <div className="bg-orange-200">
         <section className="overflow-hidden bg-[url(https://images.unsplash.com/photo-1489987707025-afc232f7ea0f?q=80&w=2670&auto=format&fit=crop)] bg-cover bg-top bg-no-repeat">
-          <div className="bg-black/50 p-8 md:p-12 lg:px-16 lg:py-24 flex flex-col items-center justify-center text-center sm:flex-row sm:text-left">
+          <div className="bg-black/50 p-8 md:p-12 lg:px-16 lg:py-24">
             <div className="text-center ltr:sm:text-left rtl:sm:text-right">
+              <h2 className="text-2xl font-bold text-white sm:text-3xl md:text-5xl">
+                Latest Shirts
+              </h2>
+
+              <p className="hidden max-w-lg text-white/90 md:mt-6 md:block md:text-lg md:leading-relaxed">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Inventore officia corporis quasi doloribus iure architecto quae
+                voluptatum beatae excepturi dolores.
+              </p>
+
               <div className="mt-4 sm:mt-8">
                 <Link
                   href={"/products/All"}
