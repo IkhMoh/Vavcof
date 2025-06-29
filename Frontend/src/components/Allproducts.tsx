02f7bf1fc0f93b2191b4bec881a43934730afc01
ikhmoh01 <ikhlefmohamed037@gmail.com>
2025-06-29 20:08:56 +0100

    improvements

--- Frontend/src/components/Allproducts.tsx
+++ Frontend/src/components/Allproducts.tsx
@@ -1,11 +1,27 @@
-import Link from "next/link";
-import React from "react";
+"use client";
 
+import Link from "next/link";
+import React, { useContext } from "react";
+import { PhotosContext } from "@/contexts/photos";
+import Image from "next/image";
 function Allproducts() {
+  const photos = useContext(PhotosContext);
+  const image = photos[2].image[0].url;
+  // const image = "https://images.unsplash.com/photo-1489987707025-afc232f7ea0f?q=80&w=2670&auto=format&fit=crop"
+  console.log(image);
   return (
     <div className="my-20">
-      <div className="bg-orange-200">
-        <section className="overflow-hidden bg-[url(https://images.unsplash.com/photo-1489987707025-afc232f7ea0f?q=80&w=2670&auto=format&fit=crop)] bg-cover bg-top bg-no-repeat">
+      <div className="">
+        <section
+        // className={`overflow-hidden bg-[url(${image})] h-96 bg-cover bg-top bg-no-repeat`}
+        >
+          <Image
+            src={photos[2].image[0].url}
+            alt={photos[2].place}
+            width={1300}
+            height={300}
+            className="rounded"
+          />
           <div className="bg-black/50 p-8 md:p-12 lg:px-16 lg:py-24">
             <div className="text-center ltr:sm:text-left rtl:sm:text-right">
               <h2 className="text-2xl font-bold text-white sm:text-3xl md:text-5xl">
--- Frontend/src/components/Hero.tsx
+++ Frontend/src/components/Hero.tsx
@@ -10,11 +10,6 @@ function Hero() {
       <div className="mx-auto w-full  px-4 py-8 sm:px-6 lg:px-8 ">
         <div className="md:flex md:justify-between">
           <div className="md:col-span-3 ">
-            {/* <img
-              src="https://images.unsplash.com/photo-1731690415686-e68f78e2b5bd?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              className="rounded "
-              alt=""
-            /> */}
             <Image
               src={photos[0].image[0].url}
               alt={photos[0].place}
