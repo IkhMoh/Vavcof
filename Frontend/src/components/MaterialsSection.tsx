14a6d607c1baa950b251f494777ebbafe043fb68
ikhmoh01 <ikhlefmohamed037@gmail.com>
2025-07-05 19:24:19 +0100

    improvements

--- Frontend/src/components/MaterialsSection.tsx
+++ Frontend/src/components/MaterialsSection.tsx
@@ -6,10 +6,9 @@ async function MaterialsSection() {
     cache: "force-cache",
   });
   const data = await res.json();
-  console.log(data);
   return (
     <section className=" bg-[var(--Light)] text-[var(--Dark)] dark:bg-[var(--Dark)] dark:text-[var(--Light)] transition-all duration-300  my-20">
-      <h1 className="text-4xl font-bold ml-2 my-2"> Food Categories </h1>
+      <h1 className="text-4xl font-bold ml-2 my-2"> Materials Categories </h1>
       <div className="px-4 py-2">
         <div className=" gap-2 flex justify-between justify-items-center w-full py-4 overflow-y-auto">
           {data.data.map((data: any) => {
