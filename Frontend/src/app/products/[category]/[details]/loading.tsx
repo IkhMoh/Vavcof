7ac6958f9382d3eee571f3b481b79a76585b981c
ikhmoh01 <ikhlefmohamed037@gmail.com>
2025-07-09 21:37:50 +0100

    adding loading for details component

--- Frontend/src/app/products/[category]/[details]/loading.tsx
+++ Frontend/src/app/products/[category]/[details]/loading.tsx
@@ -5,11 +5,21 @@ const loading = () => {
   return (
     <div>
       <div className="  rounded-md shadow-xs  transition-all duration-300  w-[400px] ">
-        <div className="  h-[400px] w-[500px]">
-          <div className=" ">
-            <Skeleton className="max-w-[280px] h-[340px]" />
+        <div className="   h-[400px] w-[500px]">
+          {/* <div className="flex "> */}
+
+          <Skeleton className="max-w-[280px] h-[340px]" />
+
+          <div className="   justify-between  items-start">
+            <Skeleton className="max-h-12 max-w-28" />
+
+            <Skeleton className="max-h-12 max-w-20" />
+
+            <Skeleton className="max-h-12 max-w-24" />
+
+            <Skeleton className="max-h-12 max-w-12" />
           </div>
-          <Skeleton count={3} className="max-h-12 max-w-28" />
+          {/* </div> */}
         </div>
       </div>
     </div>
--- Frontend/src/app/products/[category]/[details]/page.tsx
+++ Frontend/src/app/products/[category]/[details]/page.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from "react";
+import React from "react";
 import Carddetails from "@/components/Carddetails";
 
 type Params = {
