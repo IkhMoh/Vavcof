e488eab0503f181d1c48f8c93a3e61a4eef87bd9
ikhmoh01 <ikhlefmohamed037@gmail.com>
2025-07-17 12:25:43 +0100

    error

---
+++ Frontend/src/components/CategoryCard.tsx
@@ -0,0 +1,54 @@
+"use client";
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { motion } from "framer-motion";
+
+function CategoryCard({
+  image,
+  category,
+}: {
+  image: string;
+  category: string;
+}) {
+  if (!image || !category) {
+    return null;
+  }
+  if (category === "Hot Coffee") {
+    category = "HotCoffee";
+  } else if (category === "Iced Coffee") {
+    category = "IcedCoffee";
+  }
+  return (
+    <Link href={"/products/" + category} className="">
+      <motion.div
+        className="group relative block bg-black w-[250px] h-[374px] "
+        initial={{ opacity: 0, x: -50 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ delay: 2.2 }}
+      >
+        {" "}
+        <Image
+          width={250}
+          height={250}
+          alt="category image"
+          src={image}
+          className="absolute inset-0  object-cover opacity-75 transition-opacity group-hover:opacity-50"
+        />
+        <div className="relative p-3 sm:p-4 lg:p-6">
+          <p className="text-sm font-medium tracking-widest "></p>
+
+          <p className="text-xl font-bold sm:text-2xl text-[var(--Light)] ">
+            {category === "HotCoffee"
+              ? "Hot Coffee"
+              : category === "IcedCoffee"
+              ? "Iced Coffee"
+              : category}
+          </p>
+        </div>
+      </motion.div>
+    </Link>
+  );
+}
+
+export default CategoryCard;
---
+++ Frontend/src/components/CategorySection.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import CategoryCard from "./CategoryCard";
+async function CategorySection() {
+  const res = await fetch("http://localhost:1337/api/categories?populate=*", {
+    cache: "force-cache",
+  });
+  const data = await res.json();
+  return (
+    <section className=" bg-[var(--Light)] text-[var(--Dark)] dark:bg-[var(--Dark)] dark:text-[var(--Light)] transition-all duration-300  my-20">
+      <h1 className="text-4xl font-bold ml-2 my-2"> Food Categories </h1>
+      <div className="px-4 py-2 mx-6">
+        <ul className="grid gap-2 sm:grid-cols-2  lg:grid-cols-5  justify-center justify-items-center w-full py-4 overflow-y-auto">
+          {data.data.map((data: any) => {
+            const imageUrl = data.image.url;
+            return (
+              <CategoryCard
+                key={data.id}
+                image={imageUrl}
+                category={data.category}
+              />
+            );
+          })}
+        </ul>
+      </div>
+    </section>
+  );
+}
+
+export default CategorySection;
