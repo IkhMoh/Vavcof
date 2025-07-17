a4612be4bbd283fc1c1eac0efa79980fa303b3ef
ikhmoh01 <ikhlefmohamed037@gmail.com>
2025-07-17 12:07:48 +0100

    c

--- Frontend/src/app/page.tsx
+++ Frontend/src/app/page.tsx
@@ -1,6 +1,6 @@
 import Allproducts from "@/components/Allproducts";
 import MaterialsSection from "@/components/MaterialsSection";
-import CategorySection from "@/components/CategorySection";
+import CategorySection from "@/components/Categorysection";
 import Contact from "@/components/Contact";
 import Hero from "@/components/Hero";
 // import { CgLayoutGrid } from "react-icons/cg";
